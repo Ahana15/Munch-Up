d0785ef3d1d71137d5b04f1d894d3708a59cef7b
Harrish S <harrish.s@hotmail.ca>
2019-09-20 22:26:00 +0000

    output menu items onto restaurant page

--- oldhag_Webapp/server/userRoutes/restaurant1Route.js
+++ oldhag_Webapp/server/userRoutes/restaurant1Route.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../db/index");
 
+const getMenuItems = function(restaurant_id, menu_id) {
+  return db
+    .query(
+      `
+ SELECT items.name
+ FROM items
+ JOIN menus ON items.menu_id = menus.id
+ JOIN restaurants ON restaurants.id = menus.restaurant_id
+ WHERE restaurants.id = $1 AND menus.id = $2;`,
+      [restaurant_id, menu_id]
+    )
+    .then(res => {
+      console.log(res.rows);
+      return res.rows;
+    })
+    .catch(err => console.log(err));
+};
+
+exports.getMenuItems = getMenuItems;
 //Home Page Set Up
 router.get("/", (req, res) => {
-  console.log("Restaurant Home Page Hello");
-  res.render('restaurant1');
+  const items = getMenuItems(1, 1)
+    .then(items => {
+      let holder = { items: items };
+      console.log(holder);
+      res.render("restaurant1", holder);
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,29 +1,70 @@
-   INSERT INTO users (
-    name, email, password, phone_number, is_owner) 
-    VALUES (
-    'Devin Sanders', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '+14164347869', false);
-    INSERT INTO users (
-    name, email, password) 
-    VALUES (
-    'Iva Harrison', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '+14164347869', false);
+
+INSERT INTO users
+   (
+   name, email, password, phone_number, is_owner)
+VALUES
+   (
+      'Devin Sanders', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '+14164347869', false);
+INSERT INTO users
+   (
+   name, email, password)
+VALUES
+   (
+      'Iva Harrison', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '+14164347869', false);
 
 
 
-    INSERT INTO restaurants (name, cuisine, unit_number, street, city, province, post_code, country) VALUES ('Ryus Noodle Bar', japanese, 1, '669 Queen St W', 'Toronto', 'Ontario', 'M6J 1E6', 'Canada');
+INSERT INTO restaurants
+   (name, cuisine, unit_number, street, city, province, post_code, country)
+VALUES
+   ('Ryus Noodle Bar', 'japanese', 1, '669 Queen St W', 'Toronto', 'Ontario', 'M6J 1E6', 'Canada');
 
-    INSERT INTO menu (name, restaurant_id) VALUES ('lunch', 1);
-    INSERT INTO menu (name, restaurant_id) VALUES ('winter', 1);
+INSERT INTO menus
+   (name, restaurant_id)
+VALUES
+   ('lunch', 1);
+INSERT INTO menus
+   (name, restaurant_id)
+VALUES
+   ('winter', 1);
 
-    INSERT INTO items (name, menu_id, restaurant_id ) VALUES ('Tan Tan Men', 2, 1);
-    INSERT INTO items (name, menu_id, restaurant_id ) VALUES ('Spicy Miso', 2, 1);
-    INSERT INTO items (name, menu_id, restaurant_id ) VALUES ('Shoyu', 2, 1);
-    INSERT INTO items (name, menu_id, restaurant_id ) VALUES ('Shio', 2, 1);
+INSERT INTO items
+   (name, menu_id, restaurant_id )
+VALUES
+   ('Tan Tan Men', 2, 1);
+INSERT INTO items
+   (name, menu_id, restaurant_id )
+VALUES
+   ('Spicy Miso', 2, 1);
+INSERT INTO items
+   (name, menu_id, restaurant_id )
+VALUES
+   ('Shoyu', 2, 1);
+INSERT INTO items
+   (name, menu_id, restaurant_id )
+VALUES
+   ('Shio', 2, 1);
 
-    INSERT INTO items (name, menu_id, restaurant_id ) VALUES ('Nostalgic Shio', 1, 1);
-    INSERT INTO items (name, menu_id, restaurant_id ) VALUES ('Nostalgic Shoyu', 1, 1);
-    INSERT INTO items (name, menu_id, restaurant_id ) VALUES ('Miso', 1, 1);
-    INSERT INTO items (name, menu_id, restaurant_id ) VALUES ('Shoyu', 1, 1);
-    INSERT INTO items (name, menu_id, restaurant_id ) VALUES ('Shio', 1, 1);
+INSERT INTO items
+   (name, menu_id, restaurant_id )
+VALUES
+   ('Nostalgic Shio', 1, 1);
+INSERT INTO items
+   (name, menu_id, restaurant_id )
+VALUES
+   ('Nostalgic Shoyu', 1, 1);
+INSERT INTO items
+   (name, menu_id, restaurant_id )
+VALUES
+   ('Miso', 1, 1);
+INSERT INTO items
+   (name, menu_id, restaurant_id )
+VALUES
+   ('Shoyu', 1, 1);
+INSERT INTO items
+   (name, menu_id, restaurant_id )
+VALUES
+   ('Shio', 1, 1);
 
 
 
