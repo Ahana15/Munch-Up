58606df09377e743c0e7b9b6c72d901b138ab34a
Gary Chen <garychen.whc@gmail.com>
2019-09-23 22:07:32 +0000

    pair with ahana and harrish, remove the repeating data on userorderstatus

--- oldhag_Webapp/server/restaurantRoutes/restaurantPageRoute.js
+++ oldhag_Webapp/server/restaurantRoutes/restaurantPageRoute.js
@@ -9,7 +9,9 @@ router.get("/", (req, res) => {
   database
   .getRestaurantOrders(req.session.user_id)
   .then(res => {
-    return (templateVars = {orders: res});
+    let ordered = database.groupOrds(res);
+    console.log(ordered);
+    return (templateVars = {orders: ordered});
   })
   .then(templateVars => {
    templateVars.user_id = req.session.user_id;
