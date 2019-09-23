d65a37ba7581e8bbfbb785c7cd17fb2a6e23b491
Gary Chen <garychen.whc@gmail.com>
2019-09-23 23:26:41 +0000

    pair ahana, harrish restaurant order history page setting up

--- oldhag_Webapp/public/scripts/restauranthistory.js
+++ oldhag_Webapp/public/scripts/restauranthistory.js
@@ -18,3 +18,15 @@
 //     });
 //   });
 // });
+$(document).ready(function() {
+  $(".food-list").on("click", function() {
+    console.log(this);
+    console.log($(this).html().trim())
+    const childrenClass = $(this).html().trim();
+    
+    // const ID = $(this).find($(this).html())
+    // console.log(ID);
+    console.log(childrenClass);
+    $(`#${childrenClass}`).toggle();
+  });
+});
