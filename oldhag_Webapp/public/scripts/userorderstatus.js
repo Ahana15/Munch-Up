d98d432cd647a3b813bd2d313f7b4535b6108c7b
Gary Chen <garychen.whc@gmail.com>
2019-09-25 13:40:22 +0000

    changed userorderstatus display default setting, hide all the elements

--- oldhag_Webapp/public/scripts/userorderstatus.js
+++ oldhag_Webapp/public/scripts/userorderstatus.js
@@ -9,18 +9,18 @@ $(document).ready(function() {
 
   // console.log($(".status"));
 
-  const status = document.querySelectorAll(".status");
-  // console.log(status);
+  // const status = document.querySelectorAll(".status");
+  // // console.log(status);
 
-  status.forEach(function(order) {
-    // console.log($(order).parent());
-    // console.log(order);
-    console.log(order.parentElement.nextElementSibling);
+  // status.forEach(function(order) {
+  //   // console.log($(order).parent());
+  //   // console.log(order);
+  //   console.log(order.parentElement.nextElementSibling);
 
-    if (order.innerHTML === "Not Accepted" || order.innerHTML === "Accepted - In Progress") {
-      order.parentElement.nextElementSibling.style.display = "show";
-    } else {
-      order.parentElement.nextElementSibling.style.display = "none";
-    }
-  })
+  //   if (order.innerHTML === "Not Accepted" || order.innerHTML === "Accepted - In Progress") {
+  //     order.parentElement.nextElementSibling.style.display = "show";
+  //   } else {
+  //     order.parentElement.nextElementSibling.style.display = "none";
+  //   }
+  // })
 });
